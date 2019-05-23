e99cf55cb85058a7566764fd154ebd24e4e17499
Ishan Manchanda <ishanmanchanda70@gmail.com>
2019-05-23 21:16:47 +0530

    renamed index to contents

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,8 @@ extensions = [
 	'sphinx.ext.autosectionlabel',
 ]
 
+master_doc = 'contents'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
